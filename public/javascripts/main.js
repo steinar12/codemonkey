eda22acac57396f273235e196ccc1e79f862f92d
steinar12 <ssa12@hi.is>
2016-11-24 20:07:13 +0000

    Gróf uppsetning af útliti og animations.

---
+++ public/javascripts/main.js
@@ -0,0 +1,165 @@
+$(document).ready(function(){
+	for(var i = 0; i < problems.length; i++){
+		addProblem(problems[i]);
+	}
+
+	//$('.problem').css('opacity', '0').animate({opacity: 0.7});
+});
+
+function addProblem(problemInfo){
+
+	var problem = newDiv(['problem']);
+
+	var problemSimple = newDiv(['problem-simple']);
+	var problemExtension = newDiv(['problem-extension']);
+	var problemDescription = newDiv(['problem-description']);
+	var description = newDiv(['description']);
+
+	var problemDiff = newDiv(['problem-difficulty', problemInfo.difficulty.toLowerCase()]);
+	var problemTitle = newDiv(['problem-title']);
+	var problemRecord = newDiv(['problem-record']);
+	var title = newDiv(['title']);
+
+	problemDiff.text(problemInfo.difficulty);
+	title.text(problemInfo.title);
+	problemRecord.text(problemInfo.record);
+
+	problemSimple.append(problemDiff);
+	problemTitle.append(title);
+	problemSimple.append(problemTitle);
+	problemSimple.append(problemRecord);
+	description.text(problemInfo.description);
+
+	problem.attr('selectedProblem', 'false');
+
+	problemSimple.click(function(){
+		var selected = problem.attr('selectedProblem');
+
+		if(selected == 'false'){
+			problem.css('opacity', '1');
+			problem.attr('selectedProblem', 'true');
+			problemExtension.animate({height: '55vh'},300);
+			problemRecord.animate({marginTop: '12vh', height: '55vh', width: '30%'},300);
+			description.animate({marginTop: '2vh', opacity: '1'}, 300);
+		} else {
+			problem.css('opacity', '0.7');
+			problem.attr('selectedProblem', 'false');
+			problemExtension.animate({height: '0vh'},300);
+			problemRecord.animate({marginTop: '0vh', height: '12vh', width: '15%'},300);
+			description.animate({marginTop: '-5vh', opacity: '0'}, 300);
+		}
+	});
+
+	problem.hover(function(){
+		var problem = $(this);
+		if(problem.attr('selectedProblem') == 'true') return;
+		problem.css('opacity', '0.85');
+		}, function(){
+		var problem = $(this);
+		if(problem.attr('selectedProblem') == 'true') return;
+		problem.css('opacity', '0.7');
+	});
+
+	problemDescription.append(description);
+	problemExtension.append(problemDescription);
+	problem.append(problemSimple);
+	problem.append(problemExtension);
+	$('.problem-container').append(problem);
+}
+
+function newDiv(classList){
+	var div = $('<div></div>');
+
+	for(var i = 0; i < classList.length; i++){
+		div.addClass(classList[i]);
+	}
+
+	return div;
+}
+
+var problems = [
+	{
+		difficulty: 'Easy',
+		title: 'The traveling salesman',
+		record: '43ms',
+		description: 'This is a description of the problem. Read this carefully before you even attempt to solve this problem. Beware, this problem is not for babies!'
+	},
+	{
+		difficulty: 'Easy',
+		title: 'Snow white and the huntsman',
+		record: '43ms',
+		description: 'This is a description of the problem. Read this carefully before you even attempt to solve this problem. Beware, this problem is not for babies!'
+	},
+	{
+		difficulty: 'Easy',
+		title: 'Trouble in paradise',
+		record: '43ms',
+		description: 'This is a description of the problem. Read this carefully before you even attempt to solve this problem. Beware, this problem is not for babies!'
+	},
+	{
+		difficulty: 'Easy',
+		title: 'One million grasshoppers',
+		record: '43ms',
+		description: 'This is a description of the problem. Read this carefully before you even attempt to solve this problem. Beware, this problem is not for babies!'
+	},
+	{
+		difficulty: 'Easy',
+		title: 'Romeo is looking for a lover',
+		record: '43ms',
+		description: 'This is a description of the problem. Read this carefully before you even attempt to solve this problem. Beware, this problem is not for babies!'
+	},
+	{
+		difficulty: 'Medium',
+		title: 'Trees in a graveyard',
+		record: '43ms',
+		description: 'This is a description of the problem. Read this carefully before you even attempt to solve this problem. Beware, this problem is not for babies!'
+	},
+	{
+		difficulty: 'Medium',
+		title: 'Banking gone wrong',
+		record: '43ms',
+		description: 'This is a description of the problem. Read this carefully before you even attempt to solve this problem. Beware, this problem is not for babies!'
+	},
+	{
+		difficulty: 'Medium',
+		title: 'Cowboys and wizards',
+		record: '43ms',
+		description: 'This is a description of the problem. Read this carefully before you even attempt to solve this problem. Beware, this problem is not for babies!'
+	},
+	{
+		difficulty: 'Medium',
+		title: 'Which soup is the coldest?',
+		record: '43ms',
+		description: 'This is a description of the problem. Read this carefully before you even attempt to solve this problem. Beware, this problem is not for babies!'
+	},
+	{
+		difficulty: 'Medium',
+		title: 'Love and racketball',
+		record: '43ms',
+		description: 'This is a description of the problem. Read this carefully before you even attempt to solve this problem. Beware, this problem is not for babies!'
+	},
+	{
+		difficulty: 'Hard',
+		title: 'Prince of Russia',
+		record: '43ms',
+		description: 'This is a description of the problem. Read this carefully before you even attempt to solve this problem. Beware, this problem is not for babies!'
+	},
+	{
+		difficulty: 'Hard',
+		title: 'Three golden coins and a goat',
+		record: '43ms',
+		description: 'This is a description of the problem. Read this carefully before you even attempt to solve this problem. Beware, this problem is not for babies!'
+	},
+	{
+		difficulty: 'Hard',
+		title: 'Day at the zoo',
+		record: '43ms',
+		description: 'This is a description of the problem. Read this carefully before you even attempt to solve this problem. Beware, this problem is not for babies!'
+	},
+	{
+		difficulty: 'Hard',
+		title: 'Counting raindrops',
+		record: '43ms',
+		description: 'This is a description of the problem. Read this carefully before you even attempt to solve this problem. Beware, this problem is not for babies!'
+	}
+]
--- routes/clientInterface.js
+++ routes/clientInterface.js
@@ -1,14 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-var databaseInterfaceModule = require('../models/databaseInterface');
-databaseInterface = new databaseInterfaceModule();
-databaseInterface.init();
-//databaseInterface.loadInitialData();
-//databaseInterface.getScore('Siggi','Sort');
-//databaseInterface.insertScore('Siggi','Sort','1337');
-
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
