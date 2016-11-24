5fe689f917d587f8116788890e81a4ca2e7713af
dap8 <dap8@hi.is>
2016-11-24 23:30:23 +0000

    Bætti við getScores falli

--- models/databaseInterface.js
+++ models/databaseInterface.js
@@ -12,7 +12,7 @@ var databaseInterface = function() {
       if (!exists) {
         db.run("CREATE TABLE PLAYERS (_id INTEGER PRIMARY KEY, name TEXT)");
         db.run("CREATE TABLE PROBLEMS (_id INTEGER PRIMARY KEY, title TEXT, description TEXT, difficulty TEXT)");
-        db.run("CREATE TABLE SCORES (_id INTEGER PRIMARY KEY, score TEXT, _player_id INTEGER, _problem_id INTEGER,"+
+        db.run("CREATE TABLE SCORES (_id INTEGER PRIMARY KEY, score INTEGER, _player_id INTEGER, _problem_id INTEGER,"+
                " FOREIGN KEY(_player_id) REFERENCES PLAYER(_id), FOREIGN KEY(_problem_id) REFERENCES PROBLEMS(_id))");
         
         self.loadInitialData();
@@ -35,7 +35,8 @@ var databaseInterface = function() {
   };
 
 
-  self.getScore = function(player,problem) {
+
+  self.getScores = function(problem) {
 
     db.serialize(function() {
 
@@ -45,17 +46,24 @@ var databaseInterface = function() {
       /*var statement2 ='SELECT score FROM SCORES JOIN PLAYERS ON SCORES._player_id = PLAYERS._id'+
                       ' JOIN PROBLEMS ON SCORES._problem_id = PROBLEM_id WHERE PLAYERS.name = ? AND PROBLEMS.title = ?';*/
       var statement3 = 'SELECT score FROM SCORES where _player_id = ?';
+      var statement4 = 'SELECT SCORES.scores, PLAYERS.name FROM SCORES JOIN PLAYERS ON PLAYERS._id = SCORES._player_id'+
+                       ' JOIN PROBLEMS._id = SCORES._problem_id WHERE PROBLEMS.title = ?'
 
 
       //db.each(statement2, [player,problem], function(err, row) {
       //db.each(statement3, ['1'], function(err, row) {
-
-      db.each(statement, [player,problem], function(err, row) {
+      var scores = [];
+      db.each(statement4, [problem], function(err, row) {
         console.log('score: ' + row.score);
         console.log(err);
+        var score = 
+        {
+          score : row.score,
+          name : row.name
+        };
 
       }, function() {
-          console.log('completed getScore query');
+          console.log('completed getScores query');
           //
           /*db.each('SELECT * FROM SCORES', function(err,row) {
             console.log('ROW SCORE AFTER COMPLETION: '+ row.score);
--- routes/clientInterface.js
+++ routes/clientInterface.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var databaseInterfaceModule = require('../models/databaseInterface');
+var databaseInterface = new databaseInterfaceModule();
+
+databaseInterface.init();
+databaseInterface.getScores();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
